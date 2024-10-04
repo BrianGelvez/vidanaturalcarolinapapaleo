ecee5eb34f5ec7f3d72c1f535a83587d0b71e625
Brian Gelvez <briannn97@gmail.com>
2024-10-04 06:41:08 -0300

    fix: Creacion de seccion Benefits

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -1,5 +1,79 @@
+import Image from "next/image";
+
 export default function Benefits() {
-    return (
-        <div className="w-full h-52"></div>
-    )
+  return (
+    <div className="relative w-full h-auto flex flex-col gap-12 py-12 px-4 md:px-8 lg:px-10 xl:px-16">
+      {/* Div blanco que sobresale un poco en la parte inferior */}
+      <div className="absolute bottom-[-20px] left-0 w-full h-[15vh] bg-white z-10" />
+      
+      <div className="w-full flex flex-col justify-start items-start gap-10 z-20 relative">
+        <div className="w-full flex flex-col justify-start items-start">
+          <Image
+            src="/vector1.webp"
+            width={40}
+            height={40}
+            alt="icono saludable"
+          />
+          <p className="font-bold text-[18px] pt-4">Alimentación sana.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        
+        <div className="w-full flex flex-col justify-start items-start">
+          <Image
+            src="/vector2.webp"
+            width={40}
+            height={40}
+            alt="icono saludable"
+          />
+          <p className="font-bold text-[18px] pt-4">Deportismo.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        
+        <div className="w-full flex flex-col justify-start items-start">
+          <Image
+            src="/vector3.webp"
+            width={40}
+            height={40}
+            alt="icono saludable"
+          />
+          <p className="font-bold text-[18px] pt-4">Vitaminas.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        
+        <div className="w-full flex flex-col justify-start items-start">
+          <Image
+            src="/vector4.webp"
+            width={40}
+            height={40}
+            alt="icono saludable"
+          />
+          <p className="font-bold text-[18px] pt-4">Cerebro funcional.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+      
+      <div className="w-full flex flex-col justify-center items-start z-20 relative">
+        <h1 className="text-[40px] font-[700] leading-tight">Una vida más saludable.</h1>
+        <p className="py-4">For digestive health and support</p>
+        <Image
+          src="/adultoSaludable.webp"
+          width={400}
+          height={400}
+          alt="icono saludable"
+        />
+      </div>
+    </div>
+  );
 }
