0d36042c16967ea5cb2ca3a3542b1eb2b2ad72a6
Brian Gelvez <briannn97@gmail.com>
2024-10-07 09:40:10 -0300

    styles: ultimos cambios en cards testimonies

--- src/sections/testimonies/index.tsx
+++ src/sections/testimonies/index.tsx
@@ -40,6 +40,102 @@ export default function Testimonies() {
                 <p className="text-[12px]">Cordoba, Argentina</p>
               </div>
             </div>
+            {/* Testimonio 2 */}
+            <div
+              className="flex-shrink-0 bg-black/30 backdrop-blur-md text-white p-6 rounded-lg w-[300px]
+                           h-[400px] flex flex-col justify-between items-start border-[1px] border-[#252525] shadow-full bg-gradient-to-tl from-red-600/20 via-transparent"
+            >
+              <p className="text-[40px] font-bold leading-tight text-left">
+                <span className="text-red-600 text-[40px]">“</span> Ya no tengo
+                más depresión
+                <span className="text-red-600 text-[40px]">”</span>
+              </p>
+              <div className="w-full flex flex-col justify-start items-start">
+                <Image
+                  src="/yo.webp"
+                  alt="Testimonio de cliente"
+                  width={1000}
+                  height={1000}
+                  className="rounded-full object-cover w-20 h-20 border-solid border-[2px] border-red-600"
+                />
+              </div>
+              <div className="w-full flex flex-col justify-start items-start">
+                <p className="text-[12px]">Brian Gelvez</p>
+                <p className="text-[12px]">Cordoba, Argentina</p>
+              </div>
+            </div>
+            {/* Testimonio 3 */}
+            <div
+              className="flex-shrink-0 bg-black/30 backdrop-blur-md text-white p-6 rounded-lg w-[300px]
+                           h-[400px] flex flex-col justify-between items-start border-[1px] border-[#252525] shadow-full bg-gradient-to-tl from-red-600/20 via-transparent"
+            >
+              <p className="text-[40px] font-bold leading-tight text-left">
+                <span className="text-red-600 text-[40px]">“</span>baje 30 kilos
+                en una semana
+                <span className="text-red-600 text-[40px]">”</span>
+              </p>
+              <div className="w-full flex flex-col justify-start items-start">
+                <Image
+                  src="/yo.webp"
+                  alt="Testimonio de cliente"
+                  width={1000}
+                  height={1000}
+                  className="rounded-full object-cover w-20 h-20 border-solid border-[2px] border-red-600"
+                />
+              </div>
+              <div className="w-full flex flex-col justify-start items-start">
+                <p className="text-[12px]">Brian Gelvez</p>
+                <p className="text-[12px]">Cordoba, Argentina</p>
+              </div>
+            </div>
+            {/* Testimonio 1 */}
+            <div
+              className="flex-shrink-0 bg-black/30 backdrop-blur-md text-white p-6 rounded-lg w-[300px]
+                           h-[400px] flex flex-col justify-between items-start border-[1px] border-[#252525] shadow-full bg-gradient-to-tl from-red-600/20 via-transparent"
+            >
+              <p className="text-[40px] font-bold leading-tight text-left">
+                <span className="text-red-600 text-[40px]">“</span> Ahora corro
+                30 minutos más{" "}
+                <span className="text-red-600 text-[40px]">”</span>
+              </p>
+              <div className="w-full flex flex-col justify-start items-start">
+                <Image
+                  src="/yo.webp"
+                  alt="Testimonio de cliente"
+                  width={1000}
+                  height={1000}
+                  className="rounded-full object-cover w-20 h-20 border-solid border-[2px] border-red-600"
+                />
+              </div>
+              <div className="w-full flex flex-col justify-start items-start">
+                <p className="text-[12px]">Brian Gelvez</p>
+                <p className="text-[12px]">Cordoba, Argentina</p>
+              </div>
+            </div>
+            {/* Testimonio 1 */}
+            <div
+              className="flex-shrink-0 bg-black/30 backdrop-blur-md text-white p-6 rounded-lg w-[300px]
+                           h-[400px] flex flex-col justify-between items-start border-[1px] border-[#252525] shadow-full bg-gradient-to-tl from-red-600/20 via-transparent"
+            >
+              <p className="text-[40px] font-bold leading-tight text-left">
+                <span className="text-red-600 text-[40px]">“</span> Ahora corro
+                30 minutos más{" "}
+                <span className="text-red-600 text-[40px]">”</span>
+              </p>
+              <div className="w-full flex flex-col justify-start items-start">
+                <Image
+                  src="/yo.webp"
+                  alt="Testimonio de cliente"
+                  width={1000}
+                  height={1000}
+                  className="rounded-full object-cover w-20 h-20 border-solid border-[2px] border-red-600"
+                />
+              </div>
+              <div className="w-full flex flex-col justify-start items-start">
+                <p className="text-[12px]">Brian Gelvez</p>
+                <p className="text-[12px]">Cordoba, Argentina</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
