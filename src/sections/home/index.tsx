0ea4546d4552a9c36723269278de8399fa3d6bf9
Brian Gelvez <briannn97@gmail.com>
2024-10-08 11:03:33 -0300

    fix: Mejorando la aparecia del home en deskotp

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -19,21 +19,22 @@ export default function Home() {
       />
 
       {/* Contenedor para el contenido */}
-      <div className="absolute top-[190px] lg:top-0 w-full flex flex-col lg:flex-row-reverse lg:gap-6 xl:gap-36 lg:justify-center lg:items-start lg:pt-48">
-        <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start lg:mt-14">
+      <div className="absolute top-[190px] lg:top-14 w-full flex flex-col lg:flex-row-reverse lg:gap-16 lg:justify-center lg:items-start lg:pt-48">
+        <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start lg:mt-20">
           <p className="text-[15px] font-light">100% Natural</p>
-          <h1 className="text-[40px] md:text-[50px] lg:text-[55px] text-center lg:text-start text-white font-[1000] leading-tight">
+          <h1 className="text-[40px] md:text-[45px] text-center lg:text-start text-white font-[1000] leading-tight">
             SER VIDA <br />
             SER NATURALEZA <br />
             SER <span className="text-red-600">VITALIDAD</span>
           </h1>
-          <div className="w-full flex flex-col py-6 justify-center items-center lg:items-start">
-            <button className="bg-[#484848] p-4 md:text-[20px] lg:text-[22px] font-semibold rounded-sm">
+
+          <div className="w-full flex flex-col py-6 justify-center items-center lg:items-start gap-2">
+            <button className="bg-[#484848] lg:w-[262px] p-4 md:text-[18px] font-semibold rounded-sm">
               Comprar Ahora
             </button>
-            <p className="text-[10px] md:text-[12px] lg:text-[14px] py-1 text-[#8b8b8b]">
+            {/* <p className="text-[10px] md:text-[12px] lg:text-[14px] py-1 text-[#8b8b8b]">
               20% de descuento usando este botón
-            </p>
+            </p> */}
           </div>
         </div>
 
@@ -53,7 +54,9 @@ export default function Home() {
         {/* <p className="text-slate-200 underline">Ventas por mayor y menor</p> */}
         <p className="text-white text-xl lg:text-2xl xl:text-3xl">
           📦Envio{" "}
-          <span className="text-brown-secundary font-semibold text-red-600">gratis</span>{" "}
+          <span className="text-brown-secundary font-semibold text-red-600">
+            gratis
+          </span>{" "}
           a todo el pais!📦
         </p>
       </div>
