84e9f0847f7b2abb2e3ddb2f5efc1f18771114a0
Brian Gelvez <briannn97@gmail.com>
2024-10-08 10:24:20 -0300

    styles: mejorando footer

--- src/sections/footer/index.tsx
+++ src/sections/footer/index.tsx
@@ -1,29 +1,37 @@
+import Image from "next/image";
+
 export default function Footer() {
-    return (
-      <footer className="w-full bg-red-800 py-8 px-4 md:px-8 lg:px-16 text-white">
-        <div className="max-w-4xl mx-auto flex flex-col items-center text-center">
-          <h2 className="text-4xl font-semibold mb-2">Suscribite ya</h2>
-          <p className="text-sm mb-6">
-            Recibe noticias semanales de nuestros mejores productos
-          </p>
-          <div className="w-full px-8 flex">
-            <input
-              type="email"
-              placeholder="Tu mail..."
-              className="flex-grow py-2 px-3 text-black rounded-l-sm focus:outline-none"
-            />
-            <button
-              type="submit"
-              className="bg-red-600 hover:bg-red-700 py-2 px-4 rounded-r-rounded-l-sm transition duration-300"
-            >
-              ok
-            </button>
-          </div>
-          <div className="mt-8 text-xs opacity-80">
-            <p>Copyright © 2024. All rights reserved.</p>
-            <p>Powered by Fran nada.</p>
-          </div>
+  return (
+    <footer className="w-full bg-red-800 py-8 px-4 md:px-8 lg:px-16 text-white">
+      <div className="max-w-4xl mx-auto flex flex-col items-center text-center">
+        <h2 className="text-4xl font-semibold mb-2">Suscribite ya</h2>
+        <p className="text-sm mb-6">
+          Recibe noticias semanales de nuestros mejores productos
+        </p>
+        <div className="w-full px-8 flex">
+          <input
+            type="email"
+            placeholder="Tu mail..."
+            className="flex-grow py-2 px-3 text-black rounded-l-sm focus:outline-none"
+          />
+          <button
+            type="submit"
+            className="bg-red-600 hover:bg-red-700 py-2 px-4 rounded-r-rounded-l-sm transition duration-300"
+          >
+            ok
+          </button>
         </div>
-      </footer>
-    )
-  }
+        <div className="mt-8 text-xs w-full flex flex-col justify-center items-center opacity-80">
+          <p>Copyright © 2024. All rights reserved.</p>
+          <Image
+            src="/LogoVidaNaturalBlanco.webp"
+            width={1000}
+            height={1000}
+            alt="logovidanatural"
+            className="w-24 py-2 flex justify-center items-center"
+          />
+        </div>
+      </div>
+    </footer>
+  );
+}
