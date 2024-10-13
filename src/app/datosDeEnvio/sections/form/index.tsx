9dbc7e06139e372abf339ca95cab3077bd3b633c
Brian Gelvez <briannn97@gmail.com>
2024-10-13 19:59:59 -0300

    fix: creando pagina datosDeEnvio

--- src/app/datosDeEnvio/sections/form/index.tsx
+++ src/app/datosDeEnvio/sections/form/index.tsx
@@ -1,8 +1,24 @@
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 import { TbCircleNumber1Filled } from "react-icons/tb";
 
 export default function Form() {
   return (
     <div className="w-full flex flex-col p-4 gap-4">
+      <div className="w-full flex flex-col gap-2">
+        <p className="text-[22px] text-black font-semibold">Entrega</p>
+        <div className="w-full  flex flex-row border-[2px] border-[#262E5D] rounded-md">
+          <span className="w-[12%] flex justify-center items-center bg-green-primary rounded-l-md">
+            <IoIosCheckmarkCircleOutline size={24} />
+          </span>
+          <span className="w-[88%] flex flex-row justify-between gap-12 items-center rounded-r-md p-2 bg-white">
+            <p className="text-[13px] font-semibold text-black">Correo Argentino Clasico - Envio a domicilio</p>
+            <span className="flex flex-col gap-1">
+                <p className="font-bold text-[#262E5D]">Gratis</p>
+                <p className="line-through font-extralight text-[#616161]">$6.400,00</p>
+            </span>
+          </span>
+        </div>
+      </div>
       <div className="w-full p-2 flex flex-row items-center gap-4 rounded-md bg-white">
         <TbCircleNumber1Filled className="text-green-primary" size={36} />
         <p className="text-black font-semibold">Datos de contacto</p>
@@ -15,22 +31,24 @@ export default function Form() {
             </label>
             <input
               type="text"
+              id="name"
               placeholder="Nombre completo"
-              className="border-[2px] p-3 rounded-md text-black"
+              className="border-[2px] p-3 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-900 focus:border-transparent"
             />
           </span>
           <span className="w-full flex flex-col gap-2">
-            <label htmlFor="name" className="text-slate-700">
+            <label htmlFor="email" className="text-slate-700">
               Correo *
             </label>
             <input
-              type="number"
-              placeholder="correo electronico"
-              className="border-[2px] p-3 rounded-md text-black"
+              type="email"
+              id="email"
+              placeholder="Correo electrónico"
+              className="border-[2px] p-3 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-900 focus:border-transparent"
             />
           </span>
           <span className="w-full flex flex-col gap-2">
-            <label htmlFor="phone" className="text-slate-700">
+            <label htmlFor="phone" className="text-blue-900">
               Teléfono *
             </label>
             <div className="flex flex-row">
@@ -44,7 +62,7 @@ export default function Form() {
                 type="tel"
                 id="phone"
                 placeholder="Teléfono celular"
-                className="border-[2px] p-3 rounded-r-md text-black flex-grow"
+                className="border-[2px] p-3 rounded-r-md text-black flex-grow focus:outline-none focus:ring-2 focus:ring-blue-900 focus:border-transparent"
               />
             </div>
           </span>
