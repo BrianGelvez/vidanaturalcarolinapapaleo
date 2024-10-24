8372132f176e3a41bc5f0bfc707f5219284c6d6a
Brian Gelvez <briannn97@gmail.com>
2024-10-24 16:42:46 -0300

    fix: seccion benefits

---
+++ src/sections/benefits/benefitsData/index.tsx
@@ -0,0 +1,31 @@
+// BenefitsData.ts
+export const BenefitsData = [
+  {
+    image: "/vector1black.webp",
+    imageWhite: "/vector1.webp",
+    title: "Alimentación sana",
+    description:
+      "Encargado de aportar los nutrientes esenciales que tu cuerpo necesita. Mejora tu salud, te mantiene activo y lleno de energía.",
+  },
+  {
+    image: "/vector4black.webp",
+    imageWhite: "/vector4.webp",
+    title: "Cerebro funcional",
+    description:
+      "Es fundamental para mantener el cuerpo en forma, aumentando la resistencia y mejorando la calidad de vida.",
+  },
+  {
+    image: "/vector2black.webp",
+    imageWhite: "/vector2.webp",
+    title: "Bienestar físico",
+    description:
+      "Es fundamental para mantener el cuerpo en forma, aumentando la resistencia y mejorando la calidad de vida.",
+  },
+  {
+    image: "/vector3black.webp",
+    imageWhite: "/vector3.webp",
+    title: "Vitaminas",
+    description:
+      "Es fundamental para mantener el cuerpo en forma, aumentando la resistencia y mejorando la calidad de vida.",
+  },
+];
--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -1,9 +1,8 @@
-//Dialog
 import * as Dialog from "@radix-ui/react-dialog";
-
 import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
+import { BenefitsData } from "./benefitsData";
 
 export default function Benefits() {
   return (
@@ -29,62 +28,61 @@ export default function Benefits() {
             </div>
           </div>
         </div>
-        <Dialog.Root>
-          <Dialog.Trigger className="w-full flex flex-col">
-            <div className="w-full flex flex-row justify-between items-center px-4 mt-20">
-              <div className="flex flex-row justify-center items-center gap-4">
-                <Image
-                  src="/vector1.webp"
-                  width={40}
-                  height={40}
-                  alt="icono saludable"
-                  className="py-2"
-                />
-                <p className="text-[18px] font-[600]">Alimentación sana</p>
-              </div>
-              <div>
-                <IoIosArrowDown />
-              </div>
-            </div>
-          </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay className="inset-0 fixed bg-black/70 overflow-y-auto flex justify-center items-center">
-              <Dialog.Content className="w-full h-96 bg-[#1E1E1E] flex flex-col justify-start items-center rounded-2xl p-4 m-8 relative">
-                {/* Cruz de cierre */}
-                <div className="absolute top-4 right-4">
-                  <Dialog.Close
-                    asChild
-                    title="Cerrar"
-                    className="cursor-pointer"
-                  >
-                    <IoClose className="size-6 text-white-primary" />
-                  </Dialog.Close>
-                </div>
-
-                {/* Contenedor del contenido del modal */}
-                <div className="absolute top-4 w-full flex flex-col justify-center items-center gap-12 px-2">
-                  <span className="flex flex-col justify-center items-center gap-1">
+        <div className="mt-24 flex flex-col gap-2">
+          {BenefitsData.map((benefit, index) => (
+            <Dialog.Root key={index}>
+              <Dialog.Trigger className="w-full flex flex-col">
+                <div className="w-full flex flex-row justify-between items-center gap-3">
+                  <div className="flex flex-row justify-center items-center gap-4">
                     <Image
-                      src="/vector1.webp"
-                      width={45}
-                      height={45}
+                      src={benefit.imageWhite}
+                      width={35}
+                      height={35}
                       alt="icono saludable"
                       className="py-2"
                     />
-                    <p className="text-center text-[22px] font-[700]"> Alimentación sana</p>
-                  </span>
-
-                  <p className="text-center text-[18px] font-[500] text-white px-6">
-                    Encargado de aportar los nutrientes esenciales que tu cuerpo
-                    necesita. Mejora tu salud, te mantiene activo y lleno de
-                    energía.
-                  </p>
+                    <p className="text-[18px] font-[600]">{benefit.title}</p>
+                  </div>
+                  <div>
+                    <IoIosArrowDown />
+                  </div>
                 </div>
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
-        </Dialog.Root>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay
+                  className="inset-0 z-50 fixed bg-black/70 overflow-y-auto flex justify-center
+                                     items-center"
+                >
+                  <Dialog.Content className="w-full h-96 bg-white text-black flex flex-col justify-start items-center rounded-2xl p-4 m-8">
+                    <div className="flex flex-row justify-end items-end w-full">
+                      <Dialog.Close asChild title="Close">
+                        <IoClose className="size-8" />
+                      </Dialog.Close>
+                    </div>
+                    <div className="w-full flex flex-col justify-center items-center gap-4 px-2">
+                      <span className="flex flex-col justify-center items-center">
+                        <Image
+                          src={benefit.image}
+                          width={60}
+                          height={60}
+                          alt="icono saludable"
+                          className="py-2"
+                        />
+                        <p className="text-center text-[24px] font-[700]">
+                          {benefit.title}
+                        </p>
+                      </span>
+
+                      <p className="text-center text-[19px] font-[400]">
+                        {benefit.description}
+                      </p>
+                    </div>
+                  </Dialog.Content>
+                </Dialog.Overlay>
+              </Dialog.Portal>
+            </Dialog.Root>
+          ))}
+        </div>
       </div>
     </div>
   );
